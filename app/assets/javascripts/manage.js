be8806dfd89895a2607cf5bf09cf72b2fb6c3e75
fair-gz <3194909424@qq.com>
2022-08-29 17:11:22 +0800

    minor fix

--- app/assets/javascripts/manage.js
+++ app/assets/javascripts/manage.js
@@ -14,22 +14,15 @@
 //= require dastoneui/moment
 
 //= require dastoneui/daterangepicker/daterangepicker
-
 //= require dastoneui/select2/select2.min
-
 //= require dastoneui/dropzone/dropzone.min
-
 //= require dastoneui/RWD-Table-Patterns/rwd-table.min
 //= require dastoneui/pages/jquery.responsive-table.init
-
 //= require dastoneui/sweet-alert2/sweetalert2.min
-
 //= require dastoneui/datatables/dataTables.zh.cn
-
 
 //= require dastoneui/datatables/jquery.dataTables.min
 //= require dastoneui/datatables/dataTables.bootstrap5.min
-
 //= require dastoneui/datatables/dataTables.buttons.min
 //= require dastoneui/datatables/buttons.bootstrap5.min
 //= require dastoneui/datatables/jszip.min
@@ -38,9 +31,6 @@
 //= require dastoneui/datatables/buttons.html5.min
 //= require dastoneui/datatables/buttons.print.min
 //= require dastoneui/datatables/buttons.colVis.min
-
-//= require dastoneui/datatables/dataTables.responsive.min
-//= require dastoneui/datatables/responsive.bootstrap4.min
 
 //= require dastoneui/app
 //= require dastoneui/utils
