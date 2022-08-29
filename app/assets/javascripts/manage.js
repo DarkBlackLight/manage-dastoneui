7a4b6b0c55d93eab336be81402428775043e3cd1
fair-gz <3194909424@qq.com>
2022-08-29 14:35:59 +0800

    minor fix

--- app/assets/javascripts/manage.js
+++ app/assets/javascripts/manage.js
@@ -13,25 +13,23 @@
 //= require dastoneui/simplebar.min
 //= require dastoneui/moment
 
-// 插件
-// 时间选择器
 //= require dastoneui/daterangepicker/daterangepicker
-// 选择框
+
 //= require dastoneui/select2/select2.min
-// 上传文件
+
 //= require dastoneui/dropzone/dropzone.min
-// 表格
+
 //= require dastoneui/RWD-Table-Patterns/rwd-table.min
 //= require dastoneui/pages/jquery.responsive-table.init
-// 警报弹窗
+
 //= require dastoneui/sweet-alert2/sweetalert2.min
 
 //= require dastoneui/datatables/dataTables.zh.cn
 
-<!-- Required datatable js -->
+
 //= require dastoneui/datatables/jquery.dataTables.min
 //= require dastoneui/datatables/dataTables.bootstrap5.min
-<!-- Buttons examples -->
+
 //= require dastoneui/datatables/dataTables.buttons.min
 //= require dastoneui/datatables/buttons.bootstrap5.min
 //= require dastoneui/datatables/jszip.min
@@ -40,7 +38,7 @@
 //= require dastoneui/datatables/buttons.html5.min
 //= require dastoneui/datatables/buttons.print.min
 //= require dastoneui/datatables/buttons.colVis.min
-<!-- Responsive examples -->
+
 //= require dastoneui/datatables/dataTables.responsive.min
 //= require dastoneui/datatables/responsive.bootstrap4.min
 
